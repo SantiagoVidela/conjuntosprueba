8f075c31cee708e95d09073be7306606fcbed150
Santiago Videla <santiagovidela@Santiagos-MacBook-Pro.local>
2024-01-04 19:41:27 -0500

    footer perfecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,52 @@
+// module.exports = {
+//     content: ["./src/**/*.{html,js}",
+//     "./node_modules/flowbite/**/*.js",
+//     "/index.html",
+//     "/nosotros.html",
+//     "/header.html",
+//     "/voluntariado.html",
+//     "/footer.html",
+//     "/etica.html",
+//     "/cuidarnos.html",
+//     "/cosa.html",
+//     "/contigo.html",
+//     "/casaronald.html",
+//     "/app-bundle.js",
+//     "/footer.js",
+//     "/header.js",
+//     "script.js"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [
+//       require('flowbite/plugin')
+//     ],
+//     purge: {
+//         content: [
+//           // "/index.html",
+//           // "/nosotros.html",
+//           // "/header.html",
+//           // "/voluntariado.html",
+//           // "/footer.html",
+//           // "/etica.html",
+//           // "/cuidarnos.html",
+//           // "/cosa.html",
+//           // "/contigo.html",
+//           // "/casaronald.html"
+//         ],
+//       },
+// }
+
+
 module.exports = {
-    content: ["./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js",
-    "/index.html",
-    "/nosotros.html",
-    "/header.html",
-    "/voluntariado.html",
-    "/footer.html",
-    "/etica.html",
-    "/cuidarnos.html",
-    "/cosa.html",
-    "/contigo.html",
-    "/casaronald.html",
-    "/app-bundle.js",
-    "/footer.js",
-    "/header.js",
-    "script.js"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+  purge: {
+      content: [
+        "/index.html",
+      ],
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
-    purge: {
-        content: [
-          // "/index.html",
-          // "/nosotros.html",
-          // "/header.html",
-          // "/voluntariado.html",
-          // "/footer.html",
-          // "/etica.html",
-          // "/cuidarnos.html",
-          // "/cosa.html",
-          // "/contigo.html",
-          // "/casaronald.html"
-        ],
-      },
 }
